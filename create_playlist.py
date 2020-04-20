0fd7fea4ce37fd50a8662b8a6200ecc6dd6a46e6
alejandra <alejandra.casanova@upr.edu>
2020-04-19 21:07:10 -0400

    first commit

---
+++ create_playlist.py
@@ -0,0 +1,19 @@
+'''
+Tutorial: https://www.youtube.com/watch?v=7J_qcttfnJA
+Automate Spotify with python
+This tutorial aims to automatically add liked songs on youtube to a playlist on Spotify
+APIs used: Spotify We API, Youtube Data API
+'''
+
+class CreatePlaylist:
+    def __init__(self):
+        pass
+    def get_youtube_client(self):
+        pass
+    def get_liked_vides(self):
+        pass
+    def create_playlist(self):
+        pass
+    def get_spotify_uri(self):
+        pass
+    def add_song_to_playlist(self):
